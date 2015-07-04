84be861489132d53c2525de547ef88ae50c9b90a
Matt Fischer <matt@mattfischer.com>
2015-07-04 03:15:50 +0000

    Drop UCA and r10k from setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-sudo add-apt-repository cloud-archive:kilo
-sudo apt-get update 
-sudo apt-get install puppet puppet-common git r10k librarian-puppet
+sudo apt-get install puppet puppet-common git librarian-puppet

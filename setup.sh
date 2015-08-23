5fde454b6ec571429cf2d00b89d665df43287149
Matt Fischer <matt@mattfischer.com>
2015-08-23 00:48:56 +0000

    enhancements to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install puppet puppet-common git librarian-puppet
+sudo apt-get install -y puppet puppet-common git librarian-puppet
+
+sudo cp -a * /etc/puppet
+sudo service puppet restart
+
+cd /etc/puppet
+librarian-puppet install --clean

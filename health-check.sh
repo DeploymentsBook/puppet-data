a0dad15a556d6c779c2502c7ffe83c3c779ba6e7
Matt Fischer <matt@mattfischer.com>
2015-10-12 02:40:48 +0000

    basic health-check for openstack

---
+++ health-check.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# basic openstack health check
+# openrc assumed to be sourced
+
+set -x
+
+if [ -z $OS_AUTH_URL ]; then
+    echo "OS_AUTH_URL must be set"
+    exit 1
+fi
+if [ -z $OS_USERNAME ]; then
+    echo "OS_USERNAME must be set"
+    exit 1
+fi
+if [ -z $OS_PASSWORD ]; then
+    echo "OS_PASSWORD must be set"
+    exit 1
+fi
+if [ -z $OS_TENANT_NAME ]; then
+    echo "OS_TENANT_NAME must be set"
+    exit 1
+fi
+
+# Keystone
+openstack token issue
+openstack role list
+
+# Nova
+openstack host list
+openstack flavor list
+openstack server list
+
+# Glance
+openstack image list
+
+# Cinder
+openstack volume list
+
+# Ceilometer
+ceilometer alarm-list
+
+# Neutron
+openstack network list
+
+# Swift
+openstack container list

18cd61c61a7e2b9bb6c81288dd049e03e5a0279b
Elizabeth K. Joseph <lyz@princessleia.com>
2015-12-01 18:02:33 -0800

    Add required sudo commands to setup.sh
    Both the `gem install` and the `r10k puppetfile install` commands
    require sudo to work, adding it.

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 sudo apt-get update
 sudo apt-get install -y puppet puppet-common git ruby
-gem install --no-rdoc r10k
+sudo gem install --no-rdoc r10k
 
 sudo cp -a * /etc/puppet
 sudo service puppet restart
 
 cd /etc/puppet
-r10k puppetfile install -v info
+sudo r10k puppetfile install -v info
